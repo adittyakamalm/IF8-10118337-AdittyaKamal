7bf353432d03c3f9a2180b60ed8fc384ed64dc3c
adittyakamalm <35920533+adittyakamalm@users.noreply.github.com>
2020-04-08 23:17:56 +0700

    Perubahan

---
+++ Provis_Praktikum1/src/kelasutama.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aditt
+ */
+public class kelasutama {
+    public static void main(String[] args)
+    {
+        //Menghidupkan objek atau instansiasi
+        //frameutama futama = new frameutama();
+        //futama.tampil("Program Utama GUI");
+        
+        futama utama = new futama();
+        utama.setTitle("Pemograman Provis");
+        utama.setVisible(true);
+    }
+}
